871b39ee81a5c635a5645999220e746b11dff6c7
shubhampotale <34454300+shubhampotale@users.noreply.github.com>
2024-12-14 18:29:07 +0530

    Update appspec.yml

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! From Shubham'
+    return 'Hello, world! From Shubham Potale'
 
 if __name__ == '__main__':
     app.run()
